77061cda00ebd9313d6fe086b46aa11166bc4233
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-01-31 20:42:49 -0300

    Main Layout added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,17 @@
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Button } from '@nextui-org/react'
+import { Layout } from '../../components/layouts';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    
+    <Layout title={'Listado de Pokemon'} >
      <h1>Hola Mundo</h1>
-     <Button color='warning' >Click me</Button>;
-    </>
+     <Button color='warning' >Click me</Button>
+     </Layout>
+ 
   )
 }
