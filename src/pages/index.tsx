69328090a964957bc2945d2d81bc291e01021b5f
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-02 15:06:36 -0300

    PokemonCard component created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,69 +4,51 @@ import { Layout } from '../../components/layouts';
 import pokeAPi from '../../api/pokeAPi';
 import { PokemonListResponse, SmallPokemon } from '../../interfaces/pokemon-list';
 import React from 'react';
-import { Card, Grid, Image, Row,Text } from '@nextui-org/react';
+import {  Grid,} from '@nextui-org/react';
+import { PokemonCard } from 'components/pokemon';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
- interface Props {
- pokemon:SmallPokemon[];
+interface Props {
+  pokemon: SmallPokemon[];
 }
-export default function Home<SmallPokemon>({ pokemon }:Props) {
-  
-  return (
-    
-    <Layout title={'Listado de Pokemon'} >
-    <Grid.Container gap={2} justify="flex-start">
-     {
-     
-      pokemon.map((pokemon)=>(
-        <Grid xs={6} sm={3} md={2} xl={1} key={pokemon.id}> 
-          <Card isHoverable isPressable  >
-            <Card.Body css={{ p:1 }}>
-              <Card.Image
-              src={pokemon.img}
-              width='100%'
-              height={ 140 }
-              ></Card.Image>
-            </Card.Body>
-            <Card.Footer>
-              <Row justify='space-between' >
-                <Text transform='capitalize' >{pokemon.name}</Text>
-                <Text>{pokemon.id}</Text>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-      ))
-     }
-     </Grid.Container>
+export default function Home({ pokemon }:Props) {
 
-     </Layout>
- 
+  return (
+
+    <Layout title={'Listado de Pokemon'} >
+      <Grid.Container gap={2} justify="flex-start">
+        {
+
+          pokemon.map((pokemon) => (
+            <PokemonCard pokemon={ pokemon } key={ pokemon.id } />
+          ))
+        }
+      </Grid.Container>
+
+    </Layout>
+
   )
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const resp = await pokeAPi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  let pokemons: SmallPokemon[];
+  pokemons = resp.data.results.map((pokemon, index) => (
+    {
+      name: pokemon.name,
+      url: pokemon.url,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`,
+      id: index + 1
+    }
+  ))
   
-const resp = await pokeAPi.get<PokemonListResponse>('/pokemon?limit=151');
-// name: string;
-// url:  string;
-// img:string;
-// id:number;
-let pokemons : SmallPokemon[];
-pokemons = resp.data.results.map((pokemon,index) =>(
-      {
-      name:pokemon.name,
-      url:pokemon.url,
-      img:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg`,
-      id:index+1
-     }
-))
-//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg
   return {
     props: {
-      pokemon:pokemons
+      pokemon: pokemons
     }
   }
 }
