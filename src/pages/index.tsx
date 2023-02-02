01c28c736a3dd8939b3461f5670dec01173ee6e3
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-02 14:24:59 -0300

    poke data mapped in Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,33 @@ import { GetStaticProps } from 'next'
 import { Inter } from '@next/font/google'
 import { Layout } from '../../components/layouts';
 import pokeAPi from '../../api/pokeAPi';
-import { PokemonListResponse } from '../../interfaces/pokemon-list';
+import { PokemonListResponse, SmallPokemon } from '../../interfaces/pokemon-list';
+import React from 'react';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home(props:any) {
-  console.log(props)
+ interface Props {
+ pokemon:SmallPokemon[];
+}
+export default function Home<SmallPokemon>({ pokemon }:Props) {
+  
   return (
     
     <Layout title={'Listado de Pokemon'} >
-    <ul>
-      <li>Pokémon</li>
-      <li>Pokémon</li>
-      <li>Pokémon</li>
-      <li>Pokémon</li>
-      <li>Pokémon</li>
-      <li>Pokémon</li>
-    </ul>
+   
+     {
+     
+      pokemon.map((pokemon)=>(
+         <React.Fragment key={pokemon.name} >
+         <h2 key={pokemon.id+1} >{pokemon.id}</h2>
+         <p key={pokemon.id} >{pokemon.name}</p>
+         <img key={pokemon.id+2} src={pokemon.img} alt={pokemon.name} />
+         </React.Fragment>
+        
+      ))
+     }
+
      </Layout>
  
   )
@@ -28,10 +37,23 @@ export default function Home(props:any) {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   
 const resp = await pokeAPi.get<PokemonListResponse>('/pokemon?limit=151');
-console.log(resp)
+// name: string;
+// url:  string;
+// img:string;
+// id:number;
+let pokemons : SmallPokemon[];
+pokemons = resp.data.results.map((pokemon,index) =>(
+      {
+      name:pokemon.name,
+      url:pokemon.url,
+      img:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg`,
+      id:index+1
+     }
+))
+//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg
   return {
     props: {
-      pokemons:resp.data.results
+      pokemon:pokemons
     }
   }
 }
