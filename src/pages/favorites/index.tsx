208c287b12b49c4ccb616360cca1643947a736b2
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-03 13:18:51 -0300

    FavoriteCardPokemon and FavoriteCardPokemon component were created

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,10 +1,11 @@
+import {useEffect} from 'react';
+import { useState }from 'react';
 
 import { NoFavorites } from 'components/ui/NoFavorites';
 import { Layout } from '../../../components/layouts/Layout';
-import {useEffect} from 'react';
-import { useState }from 'react';
+
 import { pokemons } from 'utilities';
-import { Card, Grid } from '@nextui-org/react';
+import { FavoritePokemon } from 'components/pokemon';
 
 
 const pokemon = () => {
@@ -15,27 +16,14 @@ useEffect(()=>{
   setFavoritePokemon(pokemons());
   pokemons
 },[])
+
   return (
     <Layout title='Pokémons-Favoritos' >
 
       {
         favoritePokemon?.length === 0 ? (<NoFavorites />)
         :(
-          <Grid.Container gap={ 2 } direction='row' justify='flex-start' >
-            {
-              favoritePokemon?.map( id =>(
-                <Grid xs={ 6 } sm={ 2 } xl={ 1 }  key={ id }>
-                  <Card isHoverable isPressable css={{padding:10}} >
-                    <Card.Image
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ id }.svg`} 
-                    width={'100%'}
-                    height={140}
-                    />
-                  </Card>
-                </Grid>
-              ) )
-            }
-          </Grid.Container>
+          <FavoritePokemon favoritePokemon={favoritePokemon} />
         )
       }
      
